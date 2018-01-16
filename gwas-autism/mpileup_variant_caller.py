1d3eaa62e33d034c1205bfa5253772d5bcff0c96
Serbulent Unsal <serbulentu@gmail.com>
2018-01-16 15:32:02 +0300

    mpileup initial commit

---
+++ gwas-autism/mpileup_variant_caller.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import sys, os
+
+'''
+For referance please see;
+
+'''
+
+if len(sys.argv) != 6:
+    print ("Usage: python "+sys.argv[0]+" reference_genome_path father_bam mother_bam child_bam output_prefix")
+    print(sys.argv)
+    quit()
+
+inputBam = sys.argv[1]
+reference_genome_path = sys.argv[2]
+knownSites_indels_dbsnp = sys.argv[3]
+knownSites_indels_mills = sys.argv[4]
+knownSites_indels_1000G = sys.argv[5]
+prefix = sys.argv[6]
+shutdown = False
+
+def createAndUploadMD5(fileName):
+    md5Command = "(time md5sum " + fileName + " > " + fileName + ".md5)  1>  " \
+                 + prefix + "_md5.log 2>  " + prefix + "_md5.log"
+    print("MD5 started...")
+    print(md5Command)
+    os.system(md5Command)
+    print("MD5 upload started...")
+    os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.md5'")
+
+##############################################################################################################################
+mpileupCommand = "(time samtools mpileup -uf  " \
+                                + reference_genome_path \
+                                + " " + father_bam \
+                                + " " + mother_bam \
+                                + " " + child_bam \
+                                + " -t AD,INFO/AD,ADF,INFO/ADF,ADR,INFO/ADR,DP | bcftools call -mv -O v -o "\
+                                + prefix+".mpileup.trio.vcf  "\
+                                + " )   1>  "+prefix+"_mpileup.log  2> " \
+                                + prefix+"_mpileup.log"
+print("Mpileup started...")
+print(mpileupCommand)
+
+if os.WEXITSTATUS(os.system(mpileupCommand)) != 0:
+    os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.log'")
+    print("Shutdown")
+    if shutdown:
+        os.system("shutdown -h now")
+    quit()
+
+createAndUploadMD5(prefix+".mpileup.trio.vcf")
+print("Base Recall upload started...")
+os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.mpileup.trio.vcf'")
+
+##############################################################################################################################
+
+##############################################################################################################################
+
+os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/"+prefix)
+print("\n Upload ended Shutdown progress started...")
+if shutdown:
+    os.system("shutdown -h now")
+
+
+
+
