e6e703d2d9f22305c30c9253d7ebc1e109e0442b
Serbulent Unsal <serbulentu@gmail.com>
2018-01-22 15:08:34 +0300

    - Shift to sambamba mpileup

--- gwas-autism/mpileup_variant_caller.py
+++ gwas-autism/mpileup_variant_caller.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import sys, os
+import multiprocessing
 
 '''
 For referance please see;
 
 '''
+
+numberOfCPU = multiprocessing.cpu_count()
 
 if len(sys.argv) != 6:
     print ("Usage: python "+sys.argv[0]+" reference_genome_path father_bam mother_bam child_bam output_prefix")
@@ -29,13 +32,13 @@ def createAndUploadMD5(fileName):
     os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.md5'")
 
 ##############################################################################################################################
-mpileupCommand = "(time samtools mpileup -uf  " \
-                                + reference_genome_path \
+mpileupCommand = "(time sambamba mpileup  -t "+numberOfCPU \
+                                + " -o " + prefix+".mpileup.trio.bcf  "\
+                                +"--samtools -u -g -f  " + reference_genome_path \
                                 + " " + father_bam \
                                 + " " + mother_bam \
                                 + " " + child_bam \
-                                + " -t AD,INFO/AD,ADF,INFO/ADF,ADR,INFO/ADR,DP | bcftools call -mv -O v -o "\
-                                + prefix+".mpileup.trio.vcf  "\
+                                + " -t AD,INFO/AD,ADF,INFO/ADF,ADR,INFO/ADR,DP "\
                                 + " )   1>  "+prefix+"_mpileup.log  2> " \
                                 + prefix+"_mpileup.log"
 print("Mpileup started...")
@@ -49,10 +52,29 @@ if os.WEXITSTATUS(os.system(mpileupCommand)) != 0:
     quit()
 
 createAndUploadMD5(prefix+".mpileup.trio.vcf")
-print("Base Recall upload started...")
-os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.mpileup.trio.vcf'")
+print("Mpileup upload started...")
+os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.mpileup.trio.bcf'")
 
 ##############################################################################################################################
+bcftoolsCommand = "(bcftools call -mv -O v " \
+                                + prefix + ".mpileup.trio.bcf  " \
+                                + "  -o "\
+                                + prefix+".mpileup.trio.vcf  "\
+                                + " )   1>  "+prefix+"_bcftools.log  2> " \
+                                + prefix+"_bcftools.log"
+print("BCFTools started...")
+print(bcftoolsCommand)
+
+if os.WEXITSTATUS(os.system(bcftoolsCommand)) != 0:
+    os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.log'")
+    print("Shutdown")
+    if shutdown:
+        os.system("shutdown -h now")
+    quit()
+
+createAndUploadMD5(prefix+".mpileup.trio.vcf")
+print("BCFTools upload started...")
+os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.mpileup.trio.vcf'")
 
 ##############################################################################################################################
 
