8d11df16125f8eb2dee85ead389654d9069637a5
Serbulent Unsal <serbulentu@gmail.com>
2018-01-26 10:51:47 +0300

    Haplotype Caller script added

---
+++ gwas-autism/gatk_haploypeCaller.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import sys, os
+import multiprocessing
+
+if len(sys.argv) != 6:
+    print ("Usage: python "+sys.argv[0]+" reference_genome_path father_bam mother_bam child_bam output_prefix")
+    print(sys.argv)
+    quit()
+
+print(sys.argv)
+
+reference_genome_path = sys.argv[1]
+father_bam = sys.argv[2]
+mother_bam = sys.argv[3]
+child_bam = sys.argv[4]
+prefix = sys.argv[5]
+shutdown = False
+
+def createAndUploadMD5(fileName):
+    md5Command = "(time md5sum " + fileName + " > " + fileName + ".md5)  1>  " \
+                 + prefix + "_md5.log 2>  " + prefix + "_md5.log"
+    print("MD5 started...")
+    print(md5Command)
+    os.system(md5Command)
+    print("MD5 upload started...")
+    os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.md5'")
+
+##############################################################################################################################
+
+haplotypeCallerCommand = "(time java -jar GenomeAnalysisTK.jar -T HaplotypeCaller " \
+                                + " -R " + reference_genome_path \
+                                + " -I " + father_bam \
+                                + " -I " + mother_bam \
+                                + " -I " + child_bam \
+                                + " -o " + prefix + ".haplotypecaller.trio.vcf " \
+                                + " )   1>  "+prefix+"_haplotypecaller.log  2> " \
+                                + prefix+"_haplotypecaller.log"
+print("HaplotypeCaller started...")
+print(haplotypeCallerCommand)
+
+if os.WEXITSTATUS(os.system(haplotypeCallerCommand)) != 0:
+    os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.log'")
+    print("Shutdown")
+    if shutdown:
+        os.system("shutdown -h now")
+    quit()
+
+createAndUploadMD5(prefix + ".haplotypecaller.trio.vcf")
+print("HaplotypeCaller upload started...")
+os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/" + prefix + " --exclude '*' --include '*.mpileup.trio.bcf'")
+
+
+##############################################################################################################################
+
+os.system("time aws s3 sync . s3://rnaseq-bucket/Makrogen/KATU/"+prefix)
+print("\n Upload ended Shutdown progress started...")
+if shutdown:
+    os.system("shutdown -h now")
+
+
+
+
